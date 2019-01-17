2eaf4774684fadc0fdb79c29abc969e052f34a67
joannisbs <joannisbs@gmail.com>
2019-01-17 09:23:17 -0200

    Migration: init migration user

---
+++ src/database/migrations/20190116185626-create-users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.dropTable('Users');
+  }
+};
