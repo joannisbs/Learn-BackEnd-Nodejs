aef3b1106c94f316f19f13df8e5896c45b6d5591
joannisbs <joannisbs@gmail.com>
2019-01-17 09:23:57 -0200

    Database: add user models

---
+++ src/database/models/users.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize) => {
+  const user = sequelize.define('user', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    
+  })
+  return user
+}
