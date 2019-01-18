087c87c933b529f994cf6609941dd57372a3b622
joannisbs <joannisbs@gmail.com>
2019-01-18 14:18:07 -0200

    Database: add hash function to password

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
 
 module.exports = (sequelize) => {
   const user = sequelize.define('user', {
@@ -25,5 +26,19 @@ module.exports = (sequelize) => {
     user.hasMany(models.userRoles)
   }
 
+  user.beforeCreate(hashPassword)
+  user.beforeUpdate(hashPassword)
   return user
+}
+
+let hashPassword = function (user) {
+  if (!user.changed('password')) return
+  return new Promise(function (resolve, reject) {
+    bcrypt.hash(user.password, 10,(err, hashedPassword) =>{
+      if (err) reject(err)
+      else resolve(hashedPassword)
+    })
+  }).then(function (hashedPassword) {
+    user.password = hashedPassword
+  })
 }
