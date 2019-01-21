818e2502bd26f5db1832d051d3a96433c1d261de
joannisbs <joannisbs@gmail.com>
2019-01-21 11:21:31 -0200

    Domains: add updatebyID

--- src/domains/user/index.js
+++ src/domains/user/index.js
@@ -1,11 +1,24 @@
 const database = require ('../../database')
-
+const R = require('ramda')
 const User = database.model('user')
 
 class UserDomain {
   async add (user){
     return User.create(user)
   }
+
+  async updatebyId(userToUpdate, options = {}){
+    const { userId } = options
+
+    const userInstance = await User.findByPk(userId)
+
+    const userToUpdateData = R.omit(['id'], userToUpdate)
+
+    await userInstance.update(userToUpdateData)
+
+    return userInstance
+  }
+
 }
 
 module.exports = UserDomain
