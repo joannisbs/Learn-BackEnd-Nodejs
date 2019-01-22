8eace0808601f06f99df89c44480c1a6cacce558
joannisbs <joannisbs@gmail.com>
2019-01-22 11:43:15 -0200

    App: remove trash code

--- app.js
+++ app.js
@@ -7,33 +7,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
-
 app.get('/', controllerPad )
-// const User = sequelize.define('user', {
-//   firstName: {
-//     type: Sequelize.STRING
-//   },
-//   lastName: {
-//     type: Sequelize.STRING
-//   }
-// });
 
-// User.sync().then(() => {
-//   return User.create({
-//     firstName: 'Guilherme',
-//     lastName: 'stain'
-//   });
-// })
-
-// app.use('/api/users', async (req, res, next) => {
-//   const firstName = 'guilherme'
-//   const users = await User.findAll({ 
-//     where: { firstName },
-//     attributes: ['id', 'firstName'],
-//     limit: 5, offset: 0
-//   })
-//   res.json(users)
-// })
 
 app.listen(5000, () => console.log('runninng....\nmode ' + process.env.node_env))
