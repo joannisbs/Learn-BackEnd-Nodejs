4fc406fe243ea1e75f85fa1e4724221623a250fc
joannisbs <joannisbs@gmail.com>
2019-01-23 14:21:45 -0200

    Controller: add get test

--- src/controllers/user/index.test.js
+++ src/controllers/user/index.test.js
@@ -1,5 +1,6 @@
 const faker = require('faker')
 const request = require('../../helpers/request')
+const axios = require('axios');
 
 describe('/user add', () =>{
   
@@ -10,10 +11,10 @@ describe('/user add', () =>{
       username: faker.name.firstName()
     }
     
-    const response = await request().post('/api/user',userMock)
-    // const response = await axios.post('http://localhost:5000/api/user',userMock)
-    
-    expect(response.statusCode).toBe(500)
+    const response = await request().post('/api/user', userMock)
+    //const response = await axios.post('http://localhost:5000/api/user',userMock)
+
+    expect(response.statusCode).toBe(200)
     expect(response.body.name).toEqual(userMock.name)
     expect(response.body.username).toEqual(userMock.username)
     expect(response.body.id).toBeTruthy()
@@ -21,3 +22,21 @@ describe('/user add', () =>{
   })
 })
  
+describe('/user get', () => {
+  let userCreated = null
+
+  beforeEach( async ()=> {
+    const userMock = {
+      name: faker.name.findName(), 
+      password: faker.internet.password(),
+      username: faker.name.firstName()
+    }
+    userCreated = (await request().post('/api/user',userMock)).body
+  })
+
+  test('should return all user', async () =>{
+    const response = await request().get('/api/user')
+    expect(response.statusCode).toBe(200)
+    expect(response.body.length > 0).toBeTruthy()
+  })
+})
