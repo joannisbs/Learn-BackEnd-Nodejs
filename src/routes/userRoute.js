3e48f0a0ac9cd44c85b3eac586487818ca0c8dd4
joannisbs <joannisbs@gmail.com>
2019-01-23 15:09:01 -0200

    Route: chore GetOneById

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const userController = require('../controllers/user')
 
+router.get('/user/:id', userController.getOneUserById)
 router.post('/user', userController.newUser)
 router.get('/user', userController.getUser)
-
 module.exports = router
