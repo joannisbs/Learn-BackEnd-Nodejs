192b693d05a93e7ae111080785def44dbd39cd8e
joannisbs <joannisbs@gmail.com>
2019-01-23 14:21:59 -0200

    Routes: Add get route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const userController = require('../controllers/user')
 
 router.post('/user', userController.newUser)
+router.get('/user', userController.getUser)
 
 module.exports = router
