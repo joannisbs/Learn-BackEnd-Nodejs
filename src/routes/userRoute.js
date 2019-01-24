11cd90af8f159f9f6732cd84b22eaef316c1b9f5
joannisbs <joannisbs@gmail.com>
2019-01-24 13:47:55 -0200

    Routes: chore userRoleLink routes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userController = require('../controllers/user')
 const rolesController = require('../controllers/roles')
+const linkUserRolesController = require('../controllers/userRole');
 
 // routes of user
 router.get('/user/:id', userController.getOneUserById)
@@ -18,5 +19,12 @@ router.put('/role/:id', rolesController.updateRoles)
 
 router.post('/role', rolesController.newRoles)
 
+// routes of link-user-role
+// router.get('/role/:id', rolesController.getOneRolesById)
+// router.get('/role', rolesController.getRoles)
+
+// router.put('/role/:id', rolesController.updateRoles)
+
+router.post('/link-user-role', linkUserRolesController.newUserRoleLink)
 
 module.exports = router
