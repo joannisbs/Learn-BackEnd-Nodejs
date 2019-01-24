d5882bfe6a0bcf19c46aa42d22247855d1d0841d
joannisbs <joannisbs@gmail.com>
2019-01-24 13:47:08 -0200

    App: rename controller to route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const cors = require('cors')
-const controllerUser = require('./src/routes/userRoute')
+const routes = require('./src/routes/userRoute')
 
 const bodyParser = require('body-parser')
 
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 
-app.use('/api', controllerUser )
+app.use('/api', routes )
 
 
 app.listen(5000, () => console.log('runninng....\nmode ' + process.env.node_env))
