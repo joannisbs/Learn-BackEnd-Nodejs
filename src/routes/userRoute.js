d031bb54d72ac0eb3d6c675744bc9a41be91311b
joannisbs <joannisbs@gmail.com>
2019-01-24 09:50:23 -0200

    Routes: chore newRoles post method

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const userController = require('../controllers/user')
+const rolesController = require('../controllers/roles')
 
 router.get('/user/:id', userController.getOneUserById)
 router.get('/user', userController.getUser)
@@ -7,5 +8,8 @@ router.get('/user', userController.getUser)
 router.put('/user/:id', userController.updateUser)
 
 router.post('/user', userController.newUser)
+
+router.post('/roles', rolesController.newRoles)
+
 
 module.exports = router
